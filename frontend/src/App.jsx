dc85305f3570d503db103af510093a74a929ae59
Niranjan R <palanisamyniranjan69@gmail.com>
2025-02-04 11:39:47 +0530

    Milestone 9

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import {Routes, Route,Link} from 'react-router-dom'
 import Login from './Components/Login'
 import Signup from './Components/Signup'
 import Home from './Components/Homepage'
+import { Productform } from './Components/ProductForm'
 import './App.css'
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Route path='/login' element={<Login />} />
       <Route path='/sign-up' element={<Signup/>} />
       <Route path='/' element={<Home />} />
+      <Route path='/product' element={<Productform />}></Route>
     </Routes>
     {/* <Link to='/login'><button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Login</button></Link>
     <Link to='/sign-up'><button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Sign up</button></Link> */}
